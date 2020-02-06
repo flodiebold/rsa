907775ded2165859b546bff17636810b6b5d632f
Florian Diebold <florian.diebold@freiheit.com>
2020-02-06 20:53:51 +0100

    Key generation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 
 use std::{error::Error, io::{Read, stdin, stdout, Write}};
-use num_bigint::BigUint;
+use num_bigint::{BigInt, BigUint, Sign};
 use serde::{Deserialize, Serialize};
+use num_integer::Integer;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct PublicKey {
@@ -14,16 +15,30 @@ pub struct PrivateKey {
     d: BigUint,
 }
 
+fn modular_inverse(x: BigUint, m: BigUint) -> BigUint {
+    let m = BigInt::from_biguint(Sign::Plus, m);
+    let e = BigInt::from_biguint(Sign::Plus, x).extended_gcd(&m);
+    e.x.to_biguint().or((m + e.x).to_biguint()).unwrap()
+}
+
 fn rsa_keygen() -> (PublicKey, PrivateKey) {
-    todo!()
+    loop {
+        let p = glass_pumpkin::prime::new(512).unwrap();
+        let q = glass_pumpkin::prime::new(512).unwrap();
+        let n = &p * &q;
+        let phi = (p - 1u32) * (q - 1u32);
+        let e = BigUint::from(3u32);
+        if e.gcd(&phi) != 1u32.into() {
+            continue;
+        }
+        let d = modular_inverse(e.clone(), phi);
+        let public_key = PublicKey { e, n };
+        let private_key = PrivateKey { d };
+        return (public_key, private_key);
+    }
 }
 
 fn rsa_encrypt(message: &[u8], key: PublicKey) -> Vec<u8> {
-    let message = BigUint::from_bytes_be(&message);
-
-    let encrypted = message;
-
-    let data = encrypted.to_bytes_be();
     todo!()
 }
 

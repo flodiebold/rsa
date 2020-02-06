eed62f25cd0b535be4e13e405fdd055d9370c3fe
Florian Diebold <florian.diebold@freiheit.com>
2020-02-06 19:50:59 +0100

    Simple roundtrip through BigInt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 
+use std::{error::Error, io::{Read, stdin, stdout, Write}};
+use num_bigint::BigUint;
 
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut data = Vec::new();
 
-fn main() {
-    println!("Hello, world!");
+    stdin().lock().read_to_end(&mut data)?;
+
+    let message = BigUint::from_bytes_be(&data);
+
+    let encrypted = message;
+
+    let data = encrypted.to_bytes_be();
+
+    stdout().lock().write_all(&data)?;
+
+    Ok(())
 }
